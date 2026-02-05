fac7b280f1f00748881b87606f5804c73ecc1fa5
WantongZ <zhangwt98@gmail.com>
2026-02-05 10:54:43 +0800

    改相对路径

--- tests/wandbsweep/create_sweep.py
+++ tests/wandbsweep/create_sweep.py
@@ -1,27 +1,39 @@
 import wandb
+from pathlib import Path
+
+current_file = Path(__file__).resolve()
+
+PROJECT_ROOT = current_file.parent.parent.parent
+
+HOME_DIR = Path.home()
+
+PROGRAM_PATH = str(PROJECT_ROOT / "src/lerobot/scripts/lerobot_train.py")
+DATASET_PATH = str(HOME_DIR / "datasets/hanoi_smallleft2middle")
+
 
 sweep_config = {
     "name": "lerobot_act_sweep",
-    "program":"/home/sr/PycharmProjects/lerobot_sr/src/lerobot/scripts/lerobot_train.py",
+    "program":PROGRAM_PATH,
     "method": "bayes",
     "metric": {"name": "loss", "goal": "minimize"},
     "parameters":{
         "batch_size":{"values": [8, 16, 32]},
         "optimizer.lr":{"distribution": "log_uniform_values", "max": 1e-4, "min": 1e-6},
-        "policy.chunk_size":{"distribution": "q_uniform","max": 200, "min": 30,"q": 10},
-        "policy.kl_weight":{"distribution": "log_uniform_values","min": 1,"max": 100},
+        "policy.chunk_size":{"distribution": "q_uniform","max": 200, "min": 50,"q": 10},
+        "policy.kl_weight":{"distribution": "log_uniform_values","min": 1,"max": 20},
         "policy.temporal_ensemble_coeff":{"min":0.005,"max":0.05}
 
     },
     "early_terminate": {
         "type": "hyperband",
-        "min_iter": 2,     # Agent 会先跑 min_iter 个单位，一次 log 是一个单位（现在是 2000 个 step）
-        "eta": 3,          # 3 个里面淘汰一个
+        "min_iter": 1,     # Agent 会先跑 min_iter 个单位，一次 log 是一个单位（现在是 2000 个 step）
+        "eta": 2,          # 3 个里面淘汰一个
         },
     "command": [
         "${interpreter}",
         "${program}",
-        "--dataset.root=/home/sr/datasets/hanoi_smallleft2middle",
+ #       "--batch_size=32",
+        f"--dataset.root={DATASET_PATH}",
         "--dataset.repo_id=sr/hanoi",
         "--policy.type=act",
         "--policy.device=cuda",
@@ -33,8 +45,9 @@ sweep_config = {
         "--policy.n_action_steps=1",
         "--save_freq=2000",
         "--dataset.video_backend=pyav",
+        "--dataset.image_transforms.enable=true",
+        "--dataset.image_transforms.max_num_transforms=2",
         "--policy.use_amp=true",
-#        "--output_dir=outputs/train/hanoi_sweep/${wandb_run_id}",
         "${args}", 
     ],
 }
